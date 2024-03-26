4735c3db13df2fc3d2eec66bd46ecc5dfe954aeb
Raul <95168615+raugl@users.noreply.github.com>
2024-03-26 03:07:20 +0200

    Day 13

---
+++ src/day13/src/bin/part2.rs
@@ -0,0 +1,98 @@
+use std::cmp;
+
+fn process(input: &str) -> i64 {
+    let mut lines = input.lines().map(|line| line.trim());
+    let mut result = 0;
+
+    while let Some(lines) = parse_block(&mut lines) {
+        let row = (1..lines.len()).find(|y| test_row(&lines, *y) == 1);
+
+        if let Some(row) = row {
+            result += row * 100;
+        } else {
+            let width = lines.first().map_or(0, |line| line.len());
+            result += (1..width).find(|x| test_column(&lines, *x) == 1).unwrap();
+        }
+    }
+
+    result as i64
+}
+
+fn parse_block<'a>(input: &mut impl Iterator<Item = &'a str>) -> Option<Vec<&'a str>> {
+    let mut lines = Vec::new();
+
+    while let Some(line) = input.next() {
+        match (line.is_empty(), lines.is_empty()) {
+            (true, false) => break,
+            (true, true) => continue,
+            (false, _) => lines.push(line),
+        }
+    }
+
+    (!lines.is_empty()).then_some(lines)
+}
+
+fn diff_strs(a: &str, b: &str) -> usize {
+    a.chars().zip(b.chars()).filter(|(c1, c2)| c1 != c2).count()
+}
+
+fn test_row(lines: &Vec<&str>, y: usize) -> usize {
+    let half_count = cmp::min(y, lines.len().saturating_sub(y));
+    let top = &lines[y - half_count..y];
+    let bot = &lines[y..y + half_count];
+
+    top.iter()
+        .rev()
+        .zip(bot.iter())
+        .map(|(top, bot)| diff_strs(*top, *bot))
+        .sum()
+}
+
+fn test_column(lines: &Vec<&str>, x: usize) -> usize {
+    lines
+        .iter()
+        .map(|line| {
+            let half_count = cmp::min(x, line.len().saturating_sub(x));
+            let left = &line[x - half_count..x];
+            let right = &line[x..x + half_count];
+
+            left.chars()
+                .rev()
+                .zip(right.chars())
+                .filter(|(c1, c2)| c1 != c2)
+                .count()
+        })
+        .sum()
+}
+
+fn main() {
+    let input = include_str!("input.txt");
+    println!("Result: {}", process(input));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_process() {
+        let input = "
+            #.##..##.
+            ..#.##.#.
+            ##......#
+            ##......#
+            ..#.##.#.
+            ..##..##.
+            #.#.##.#.
+
+            #...##..#
+            #....#..#
+            ..##..###
+            #####.##.
+            #####.##.
+            ..##..###
+            #....#..#
+        ";
+        assert_eq!(process(input), 400);
+    }
+}
